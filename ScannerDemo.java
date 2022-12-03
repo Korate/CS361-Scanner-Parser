1fd84fae2c88b59d36b670c6994470a74473ed6d
Xiaofeng <korate.qwq@gmail.com>
2022-12-02 21:29:42 -0500

    new modify by Xiaofeng

---
+++ ScannerDemo.java
@@ -0,0 +1,22 @@
+
+/**
+ * @author scharffc
+ * 
+ */
+public class ScannerDemo {
+
+	// Oh! This needs to be changed !
+	private static String file1 = "C:\\Users\\13447\\Downloads\\AutumnClass\\±‡≥Ã”Ô—‘\\hw4\\prog1.kay";
+	private static int counter = 1;
+
+	public static void main(String args[]) {
+		TokenStream ts = new TokenStream(file1);
+		System.out.println(file1);
+		Token tk;
+		while (!ts.isEndofFile()) {
+			// TODO TO BE COMPLETED
+			// tk is not defined on the line below
+			// System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
+		}
+	}
+}
